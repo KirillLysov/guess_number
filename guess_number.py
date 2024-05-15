d835df10c3c6886806ec5da1c55708eb1eb11304
Kirill Lysov <cyrillicns@gmail.com>
2024-05-15 20:44:07 +0500

    Ваше > Твое

--- guess_number.py
+++ guess_number.py
@@ -6,9 +6,9 @@ num_random = random.randint(1, 100)
 while True:
     num_input = int(input())
     if num_random > num_input:
-        print('Ваше число меньше того, что загадано')
+        print('Твое число меньше того, что загадано')
     elif num_random < num_input:
-        print('Ваше число больше того, что загадано')
+        print('Твое число больше того, что загадано')
     else:
-        print('Отличная интуиция! Вы угадали число :)')
+        print('Отличная интуиция! Ты угадал число :)')
         break
